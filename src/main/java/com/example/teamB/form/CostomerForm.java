c77e8e74a6933e263042f2df81d3514525e794b0
higuchi3030932 <kyo.hei.hei.hei@ezweb.ne.jp>
2023-09-07 15:25:42 +0900

    Form追加　樋口

--- src/main/java/com/example/teamB/form/CostomerForm.java
+++ src/main/java/com/example/teamB/form/CostomerForm.java
@@ -1,5 +1,35 @@
 package com.example.teamB.form;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CostomerForm {
+
+	//顧客リストテーブルへのForm
+	//顧客ID
+	@Id
+	private Integer id;
+
+	//顧客名
+	@NotBlank
+	private String name;
+
+	//メアド
+	@NotBlank
+	private String mail;
+
+	//パスワード
+	@NotBlank
+	private String pass;
+
+	//メモ
+	private String memo;
 
 }
--- src/main/java/com/example/teamB/form/SpotForm.java
+++ src/main/java/com/example/teamB/form/SpotForm.java
@@ -1,5 +1,25 @@
 package com.example.teamB.form;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SpotForm {
 
+	//観光スポットリストテーブルへのテーブル
+	//観光地連番
+	@Id
+	private Integer spotId;
+
+	//観光地名
+	@NotBlank
+	private String spotName;
+
+	//観光地住所
 }
