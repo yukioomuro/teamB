6175866335a0b86ae77b139574949cf921945769
higuchi3030932 <kyo.hei.hei.hei@ezweb.ne.jp>
2023-09-07 15:58:13 +0900

    entity追加　樋口

---
+++ src/main/java/com/example/teamB/entity/Purpose.java
@@ -0,0 +1,25 @@
+package com.example.teamB.entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Purpose {
+
+	//目的地リストへのForm
+	//ID
+	@Id
+	@NotBlank
+	private Integer purposeId;
+
+	//目的
+	@NotBlank
+	private String purposeKinds;
+
+}
