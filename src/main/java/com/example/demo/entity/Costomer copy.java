d5f3926eb4f728620c0b3a420174fd96fc2d2c11
yukioomuro <yuki.oomuro@gmail.com>
2023-09-19 14:46:19 +0900

    Merge branch 'master' of https://github.com/yukioomuro/teamB.git oo

---
+++ src/main/java/com/example/demo/entity/Costomer copy.java
@@ -0,0 +1,40 @@
+
+
+package com.example.demo.entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//顧客リストテーブル
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Costomer {
+
+	//顧客ID
+	@Id
+	private Integer id;
+
+	//名前
+	private String name;
+
+	//メアド
+	private String mail;
+
+	@NotBlank
+	private String mailView;
+
+	//パスワード
+	private String pass;
+
+	@NotBlank
+	private String passView;
+
+	//メモ
+	private String memo;
+}
