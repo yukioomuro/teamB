5bdbe070d4d96078e6bc4f15c7439d0f9bb47fa9
higuchi3030932 <kyo.hei.hei.hei@ezweb.ne.jp>
2023-09-27 11:07:12 +0900

    ログイン成功　樋口

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http.formLogin(form -> form
 				.loginPage("/oritabi/manager_login") // ログイン画面のURL
@@ -39,10 +41,10 @@ public class SecurityConfig {
 		return http.build();
 	}
 
-//	@Bean
-//	PasswordEncoder passwordEncoder() {
-//	//	System.out.println(new BCryptPasswordEncoder().encode("123"));
-//		return new BCryptPasswordEncoder();
-//	}
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		//System.out.println(new BCryptPasswordEncoder().encode("123"));
+		return new BCryptPasswordEncoder();
+	}
 
 }
